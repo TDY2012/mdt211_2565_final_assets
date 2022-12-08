3cf924227b10cb7aeb1484422c07f00761e5cb7a
Thitiwudh Uasmith <thitiwudh.u@mail.kmutt.ac.th>
2022-12-09 00:44:50 +0700

    feat: Added predefined data structure classes for final exam.

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("You may use any class in this project for the final examination.");
+    }
+}
